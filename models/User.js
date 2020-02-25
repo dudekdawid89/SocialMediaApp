ce2ffd6989272a38b8ecc56491d3334cbb6d739c
Dawid <dudekdawid89@gmail.com>
2020-02-24 21:23:56 -0600

    Completed checking if the registration property is already taken

--- models/User.js
+++ models/User.js
@@ -37,30 +37,52 @@ User.prototype.login = function(){
 }
 
 User.prototype.validate = function(){
-    if(this.data.username == ""){this.errors.push("You must provide a username")}
-    if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){this.errors.push('Username can only constains letters and numbers')}
-    if(!validator.isEmail(this.data.email)){this.errors.push("You must provide a email")}
-    if(this.data.password == ""){this.errors.push("You must provide a password")}
-    if(this.data.password.length > 0 && this.data.password.length < 12){this.errors.push('Password must be at least 12 characters long')}
-    if(this.data.password.length > 50){this.errors.push('Password can not exceed 50 characters')} // password set to 50 character because of bcrypt limit of characters
-    if(this.data.username.length > 0 && this.data.password.length < 3){this.errors.push('Username must be at least 3 characters long')}
-    if(this.data.username.length > 30){this.errors.push('Username can not exceed 30 characters')}
+    return new Promise(async(resolve, reject) => {
+        if(this.data.username == ""){this.errors.push("You must provide a username")}
+        if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){this.errors.push('Username can only constains letters and numbers')}
+        if(!validator.isEmail(this.data.email)){this.errors.push("You must provide a email")}
+        if(this.data.password == ""){this.errors.push("You must provide a password")}
+        if(this.data.password.length > 0 && this.data.password.length < 12){this.errors.push('Password must be at least 12 characters long')}
+        if(this.data.password.length > 50){this.errors.push('Password can not exceed 50 characters')} // password set to 50 character because of bcrypt limit of characters
+        if(this.data.username.length > 0 && this.data.username.length < 3){
+            console.log('this.data.username.length :', this.data.username.length);
+            this.errors.push('Username must be at least 3 characters long')}
+        if(this.data.username.length > 30){this.errors.push('Username can not exceed 30 characters')}
+    
+        // Only if username is valid then check to see if it's already taken
+        if(this.data.username.length > 2 && this.data.username.length < 31 && validator.isAlphanumeric(this.data.username)){
+            let usernameExists = await userCollection.findOne({username: this.data.username})
+            if(usernameExists) {this.errors.push('That username is already taken')}
+        }
+    
+            // Only if email is valid then check to see if it's already taken
+        if(validator.isEmail(this.data.email)){
+            let emailExists = await userCollection.findOne({email: this.data.email})
+            if(emailExists) {this.errors.push('That email is already taken')}
+        }
+        resolve()
+    })
 }
 
+
 User.prototype.register = function(){
-    // Step #1: Validatae user data
-    this.cleanUp()
-    this.validate()
+    return new Promise(async (resolve, reject) =>{
+        // Step #1: Validatae user data
+        this.cleanUp()
+        await this.validate()
+        // Step #2: Only if there are no validation errprs then save the user data into a database
+        console.log(this.data)
+        if(!this.errors.length){
+            // hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
+            await userCollection.insertOne(this.data)
+            resolve()
+        } else {
+            reject(this.errors)
 
-
-    // Step #2: Only if there are no validation errprs then save the user data into a database
-    console.log(this.data)
-    if(!this.errors.length){
-        // hash user password
-    let salt = bcrypt.genSaltSync(10)
-    this.data.password = bcrypt.hashSync(this.data.password, salt)
-        userCollection.insertOne(this.data)
-    }
+        }
+    })
 }
 
 
